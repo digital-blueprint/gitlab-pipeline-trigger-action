267c9e858db2910b7ae2d96e4eab651156782977
Patrizio Bekerle <patrizio@bekerle.com>
2023-03-14 15:06:13 +0100

    feat: do sample api request

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
 try {
     // `id` input defined in action metadata file
     const id = core.getInput('id');
     console.log(`Hello ${id}!`);
+
+    axios.get('https://api.github.com/users/pbek')
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
+
     const time = (new Date()).toTimeString();
     core.setOutput("status", time);
     // Get the JSON webhook payload for the event that triggered the workflow
