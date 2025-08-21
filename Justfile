ce55491856ba8da002921e4d0ff668c23f5c714b
Patrizio Bekerle <patrizio@bekerle.com>
2025-08-21 13:09:57 +0200

    devenv: add tooling and treefmt
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- Justfile
+++ Justfile
@@ -7,6 +7,8 @@
 default:
     @just --list
 
+alias fmt := format
+
 # Build the project using npm
 [group('dev')]
 build:
@@ -18,3 +20,20 @@ build:
 push-tag-v1:
     git tag -fa v1 -m "Update v1 tag"
     git push origin v1 --force
+
+# Format all files using treefmt
+[group('linter')]
+format args='':
+    treefmt {{ args }}
+
+# Format all files using pre-commit
+[group('linter')]
+format-all args='':
+    pre-commit run --all-files {{ args }}
+
+# Add git commit hashes to the .git-blame-ignore-revs file
+[group('linter')]
+add-git-blame-ignore-revs:
+    git log --pretty=format:"%H" --grep="^lint" >> .git-blame-ignore-revs
+    sort .git-blame-ignore-revs | uniq > .git-blame-ignore-revs.tmp
+    mv .git-blame-ignore-revs.tmp .git-blame-ignore-revs
---
+++ devenv.nix
@@ -0,0 +1,51 @@
+# devenv.nix
+# Nix development environment configuration
+
+{ pkgs, ... }:
+{
+  languages = {
+    javascript.enable = true;
+    nix.enable = true;
+  };
+
+  # Custom scripts available in the dev environment
+  scripts = {
+    # Shows the current Node.js version and a welcome message with emotes
+    welcome.exec = ''
+      echo "👋 Welcome to the GitLab Pipeline Trigger Action project!"
+      echo "🟢 Current Node.js version: $(node --version 2>/dev/null || echo 'Node.js not found')"
+    '';
+  };
+
+  enterShell = ''
+    welcome
+  '';
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+    eslint.enable = true;
+
+    # https://devenv.sh/reference/options/#git-hookshookstreefmt
+    # https://github.com/numtide/treefmt
+    # https://github.com/numtide/treefmt-nix
+    treefmt = {
+      enable = true;
+      settings.formatters = with pkgs; [
+        nodePackages.prettier
+        nixfmt-rfc-style
+        statix
+        taplo
+        just
+      ];
+    };
+
+    # https://devenv.sh/reference/options/#git-hookshooksdeadnix
+    # https://github.com/astro/deadnix
+    deadnix = {
+      enable = true;
+      settings = {
+        edit = true; # Allow to edit the file if it is not formatted
+      };
+    };
+  };
+}
