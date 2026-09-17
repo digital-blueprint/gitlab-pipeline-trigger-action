61de9f4a8675aa0b748be91df5748dea61b354bf
Christoph Reiter <reiter.christoph@gmail.com>
2026-09-17 10:50:41 +0200

    Add prettier dep
    devenv checks it, so make sure we also provide it with bare npm

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,38 +1,38 @@
-import { defineConfig } from "rolldown";
-import sbom from "rollup-plugin-sbom";
+import {defineConfig} from 'rolldown';
+import sbom from 'rollup-plugin-sbom';
 
 export default defineConfig({
-  input: {
-    index: "index.js"
-  },
-  output: {
-    dir: "dist",
-    format: "esm",
-    minify: true,
-    cleanDir: true,
-  },
-  platform: "node",
-  plugins: [
-    sbom({
-      includeWellKnown: false,
-      outDir: "",
-      outFilename: "index.js.cdx",
-      saveTimestamp: false,
-      afterCollect(bom) {
-        for (const tool of bom.metadata.tools.tools) {
-          tool.version = undefined;
-        }
-      },
-    }),
-    {
-      name: "emit-package-json",
-      generateBundle() {
-        this.emitFile({
-          type: "asset",
-          fileName: "package.json",
-          source: JSON.stringify({ type: "module" }),
-        });
-      },
+    input: {
+        index: 'index.js',
     },
-  ],
+    output: {
+        dir: 'dist',
+        format: 'esm',
+        minify: true,
+        cleanDir: true,
+    },
+    platform: 'node',
+    plugins: [
+        sbom({
+            includeWellKnown: false,
+            outDir: '',
+            outFilename: 'index.js.cdx',
+            saveTimestamp: false,
+            afterCollect(bom) {
+                for (const tool of bom.metadata.tools.tools) {
+                    tool.version = undefined;
+                }
+            },
+        }),
+        {
+            name: 'emit-package-json',
+            generateBundle() {
+                this.emitFile({
+                    type: 'asset',
+                    fileName: 'package.json',
+                    source: JSON.stringify({type: 'module'}),
+                });
+            },
+        },
+    ],
 });
